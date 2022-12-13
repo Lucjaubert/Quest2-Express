095ac54a0eb06fc749e8f0aa845230fd6603a7f7
Luc Jaubert <lucjaubert@gmail.com>
2022-12-13 12:58:39 +0100

    Quete3

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5001;
 
 const welcome = (req, res) => {
@@ -17,9 +19,13 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+app.post("/api/movies", movieHandlers.postMovie);
+
 const userHandlers = require("./userHandlers");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/users", userHandlers.postUsers);
 
 app.listen(port, (err) => {
   if (err) {
@@ -28,3 +34,4 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
